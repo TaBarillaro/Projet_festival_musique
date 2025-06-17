c009661fdab7f8c230a48c57d3144c794fb34d07
tania.barillaro <nanacampus2025@gmail.com>
2025-06-17 16:09:52 +0200

    Modification liste-des-artistes html et ts pour rendre la liste plus dynamique

--- src/app/liste-des-artistes/liste-des-artistes.ts
+++ src/app/liste-des-artistes/liste-des-artistes.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-liste-des-artistes',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './liste-des-artistes.html',
   styleUrl: './liste-des-artistes.scss'
 })
 export class ListeDesArtistes {
-
+  artistes = [
+    {nom: 'Giulia Rossi', image: "/images/artiste2.jpg"},
+    {nom: 'Marco Bianchi', image: "/images/artiste1.jpg"},
+    {nom: 'Elena Ferraro', image: "/images/artiste3.jpg"},
+  ]
 }
