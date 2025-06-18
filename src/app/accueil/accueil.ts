0525d0054170d7fcaa8eae7e48a1b146d6d0afec
Tania BARILLARO <tania.barillaro@Digital-Grenoble.local>
2025-06-18 11:40:13 +0200

    ajout logique if pour afficher/masquer les elements

--- src/app/accueil/accueil.ts
+++ src/app/accueil/accueil.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class Accueil {
 
+  showDescription = false;
+
+  afficheDescription() {
+      this.showDescription = !this.showDescription;
+    }
 }
