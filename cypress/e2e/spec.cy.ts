fb12aef2a101b757cca256f03a170d6042f241b4
tania.barillaro <nanacampus2025@gmail.com>
2025-06-23 14:24:34 +0200

    test ajout artiste dans la liste des artistes

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,18 @@
-describe('My first test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(false)
+describe('Page d\'accueil', () => {
+  it('successfully loads', () => {
+    cy.visit('/')
+
+    cy.contains('Liste des artistes').click()
+
+    cy.url().should('include', '/liste-artistes')
+
+    cy.get('#name').type('Monsieur l\'insect')
+    
+    cy.get('#photo').type('https://cdn.pixabay.com/photo/2025/05/01/16/21/insect-9571817_640.jpg')
+
+    cy.contains('Ajouter').click()
+
+    cy.contains('Monsieur l\'insect').should('exist')
+
   })
 })
